cb7803e0c060a3674aa289c766bdce84268ab8fe
Donta Smith <dnovsmith.ds20@gmail.com>
2024-10-17 20:31:16 +0000

    added some minor animations to social media icons

--- article-preview-component/main.js
+++ article-preview-component/main.js
@@ -1,5 +1,5 @@
 const shareButton = document.querySelector(".share__button");
-
+const imgIcon = shareButton.querySelector(".icon");
 const shareButtonActive = document.querySelector(".share__button--active");
 
 shareButton.addEventListener("click", () => {
@@ -11,5 +11,13 @@ shareButton.addEventListener("click", () => {
 shareButton.addEventListener("click", () => {
   if (window.innerWidth >= 768) {
     shareButtonActive.classList.toggle("active");
+
+    if (shareButtonActive.classList.contains("active")) {
+      shareButton.style.background = "hsl(214, 17%, 51%)";
+      imgIcon.src = "./images/icon-share-white.svg";
+    } else {
+      shareButton.style.background = ""; // Reset to default
+      imgIcon.src = "./images/icon-share.svg";
+    }
   }
 });
